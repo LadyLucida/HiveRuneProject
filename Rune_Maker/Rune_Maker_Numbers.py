c39cab65113b36f172a5208114f2a473005a6c55
LadyLucida <LadyLucida@gmail.com>
2022-04-13 21:16:26 -0700

    added number maker

--- Rune_Maker/Rune_Maker_Numbers.py
+++ Rune_Maker/Rune_Maker_Numbers.py
@@ -1,5 +1,7 @@
+from cmath import nan
 import os
 import csv
+from tokenize import Triple
 import numpy as np
 import cv2
 import pandas as pd
@@ -9,7 +11,6 @@ def fix(folder):
     dir = "../Parts/Numbers/"
     name = os.listdir(dir + folder)
     name = sorted(name)
-    name = [s.strip('.png') for s in name] # remove the png from the string borders
     name = [s.replace('.png', '') for s in name] # remove all the png
     return name
 
@@ -60,239 +61,117 @@ def Variant(data):
     variant = []
         
     for index,row in data.iterrows():
-        
-        top = row['tops']
-        mid = row['mids']
-        bot = row['bots']
-        
-        print("Original:",[top,mid,bot])        
+        search = row.tolist()
+        if np.nan in search:
+            v = [a for a in search if pd.isnull(a)== False]
+            f = v[::-1]
+            if f == v:
+                print(search)
+                print('This rune cannot be flipped')
+                continue
+            variant.append(f)
+        else:
+            s = search[::-1]
+            if s == search:
+                print(search)
+                print('This rune cannot be flipped')
+                continue
+            variant.append(s)
 
-        tnum = int(top.rpartition('_')[-1])
-        mnum = int(mid.rpartition('_')[-1])
-        bnum = int(bot.rpartition('_')[-1])
-        
-        if mnum == 1:
-            mnew = 1
-        if mnum == 2:
-            mnew = 5
-        if mnum == 3:
-            mnew = 3
-        if mnum == 4:
-            mnew = 4
-        if mnum == 5:
-            mnew = 2
-        if mnum == 6:
-            mnew = 7
-        if mnum == 7:
-            mnew = 6
-        
-        
-        tnew_v = Getnumber(tnum)
-        bnew_v = Getnumber(bnum)
-        
-        
-        
-        top_v = 'Top_' + str(tnew_v).zfill(2)
-        mid_v = 'Mid_' + str(mnew).zfill(2)
-        bot_v = 'Bot_' + str(bnew_v).zfill(2)
-        
-        
-        
-        print("Vertical Variant:",[top_v,mid_v,bot_v])        
-        if ((data['tops'] == top_v) & (data['mids'] == mid_v) & (data['bots'] == bot_v)).any() == True:
-            print("VV exists!")
-        elif [top_v,mid_v,bot_v] in variant:
-            print("VV already in variant list")
-        else:
-            print("adding VV variant list")
-            variant.append([top_v,mid_v,bot_v])
-            
-        tnew_h = bnum
-        bnew_h = tnum
-          
-        top_h = 'Top_' + str(tnew_h).zfill(2)
-        mid_h = 'Mid_' + str(mnew).zfill(2)
-        bot_h = 'Bot_' + str(bnew_h).zfill(2)
+    
+    for i in variant:
+        print(i)
 
-        print("Horizontal Variant:",[top_h,mid_h,bot_h])
-        if ((data['tops'] == top_h) & (data['mids'] == mid_h) & (data['bots'] == bot_h)).any() == True:
-            print("HV exists!")
-        elif [top_h,mid_h,bot_h] in variant:
-            print("HV already in variant list")
-        else:
-            print("adding HV variant list")
-            variant.append([top_h,mid_h,bot_h])
-            
-            
-        tnew_vh = Getnumber(tnew_h)
-        bnew_vh = Getnumber(bnew_h)
-        mnew_vh = mnum
-        
-        top_vh = 'Top_' + str(tnew_vh).zfill(2)
-        mid_vh = 'Mid_' + str(mnew_vh).zfill(2)
-        bot_vh = 'Bot_' + str(bnew_vh).zfill(2)
-        
-       
-        print("Vertical + Horizontal Variant:",[top_vh,mid_vh,bot_vh])
-        if ((data['tops'] == top_vh) & (data['mids'] == mid_vh) & (data['bots'] == bot_vh)).any() == True:
-            print("VHV exists!")
-            print()
-        elif [top_vh,mid_vh,bot_vh] in variant:
-            print("VHV already in variant list")
-            print()
-        else:
-            print("adding VHV variant listS")
-            variant.append([top_vh,mid_vh,bot_vh])     
-            print()
-            
     return variant
 
-       
-dir = "../Parts/Full/"
-top = fix("Nums/")
+def SaveFunction(data,search,save_name,img,variant,short = True):
+
+
+    for r in data:
+        print('Search:',search)
+        if short == True:
+            if ((data['num0'] == search[0]) & (data['mid0'] == search[1]) & (data['num1'] == search[2])).any() == True:
+                print("This is a Real Rune")
+                print()
+                real = color_change(img,real=True)
+                save_dir = 'Numbers/Real/'
+                cv2.imwrite( save_dir + save_name,real)
+            else:
+                if search in variant:
+                    print("This is a Variant Rune")
+                    print()
+                    flip = color_change(img,flip=True)
+                    save_dir = 'Numbers/Variant/'
+                    cv2.imwrite( save_dir + save_name,flip)
+                else:
+                    print("This is a Hypothetical Rune")
+                    print()
+                    hypo = color_change(img,hypo=True)      
+                    save_dir = 'Numbers/Hypothetical/'
+                    cv2.imwrite( save_dir + save_name,hypo)
+
+        if short == False:
+            if ((data['num0'] == search[0]) & (data['mid0'] == search[1]) &
+                (data['num1'] == search[2]) & (data['mid1'] == search[3]) & 
+                (data['num2'] == search[4])).any() == True:
+                print("This is a Real Rune")
+                print()
+                real = color_change(img,real=True)
+                save_dir = 'Numbers/Real/'
+                cv2.imwrite( save_dir + save_name,real)
+            else:
+                if search in variant:
+                    print("This is a Variant Rune")
+                    print()
+                    flip = color_change(img,flip=True)
+                    save_dir = 'Numbers/Variant/'
+                    cv2.imwrite( save_dir + save_name,flip)
+                else:
+                    if search[2] == 'num_00':
+                        print("This rune breaks the rules")
+                        continue
+                    else:
+                        print("This is a Hypothetical Rune")
+                        print()
+                        hypo = color_change(img,hypo=True)      
+                        save_dir = 'Numbers/Hypothetical/'
+                        cv2.imwrite( save_dir + save_name,hypo)         
+
+dir = "../Parts/Numbers/"
+num = fix("Nums/")
 mid = fix("Mids/")
-
-broken_top = [
-    'Top_13','Top_14','Top_15','Top_16','Top_17','Top_18','Top_19','Top_20','Top_21','Top_22',
-    'Top_23','Top_24','Top_28','Top_29','Top_33','Top_34','Top_35','Top_36','Top_37','Top_38',
-    'Top_39','Top_40','Top_41','Top_42','Top_43','Top_44','Top_45','Top_46','Top_47','Top_48',
-    'Top_49','Top_50','Top_51','Top_52','Top_53','Top_54','Top_55','Top_56','Top_60','Top_61',
-    'Top_62','Top_63','Top_64','Top_65','Top_66','Top_67','Top_69','Top_70','Top_71','Top_72',         
-    'Top_73','Top_74','Top_75','Top_76','Top_77','Top_78','Top_79','Top_80','Top_81',               
-]
-                
-broken_bot = [
-    'Bot_13','Bot_14','Bot_15','Bot_16','Bot_17','Bot_18','Bot_19','Bot_20','Bot_21','Bot_22',
-    'Bot_23','Bot_24','Bot_28','Bot_29','Bot_33','Bot_34','Bot_35','Bot_36','Bot_37','Bot_38',
-    'Bot_39','Bot_40','Bot_41','Bot_42','Bot_43','Bot_44','Bot_45','Bot_46','Bot_47','Bot_48',
-    'Bot_49','Bot_50','Bot_51','Bot_52','Bot_53','Bot_54','Bot_55','Bot_56','Bot_60','Bot_61',
-    'Bot_62','Bot_63','Bot_64','Bot_65','Bot_66','Bot_67','Bot_69','Bot_70','Bot_71','Bot_72',         
-    'Bot_73','Bot_74','Bot_75','Bot_76','Bot_77','Bot_78','Bot_79','Bot_80','Bot_81',               
-]
-                
-circle_top = [
-    'Top_04','Top_05','Top_17','Top_18','Top_19','Top_34','Top_38','Top_39','Top_40','Top_31',
-    'Top_44','Top_45','Top_58','Top_61','Top_65','Top_66','Top_67','Top_71','Top_72',
-]
-
-circle_bot = [
-    'Bot_04','Bot_05','Bot_17','Bot_18','Bot_19','Bot_34','Bot_38','Bot_39','Bot_40','Bot_31',
-    'Bot_44','Bot_45','Bot_58','Bot_61','Bot_65','Bot_66','Bot_67','Bot_71','Bot_72',
-]      
-
-triangle_top = [
-    'Top_05','Top_08''Top_18','Top_19','Top_23','Top_24','Top_25','Top_38','Top_39','Top_40',
-    'Top_49','Top_50','Top_51','Top_65','Top_66','Top_67','Top_76','Top_77','Top_78',
-]
-
-triangle_bot = [
-    'Bot_05','Bot_08''Bot_18','Bot_19','Bot_23','Bot_24','Bot_25','Bot_38','Bot_39','Bot_40',
-    'Bot_49','Bot_50','Bot_51','Bot_65','Bot_66','Bot_67','Bot_76','Bot_77','Bot_78',
-]
-
-real_top = [
-    'Top_01','Top_02','Top_03','Top_04','Top_05','Top_06','Top_07','Top_08','Top_09','Top_10',
-    'Top_11','Top_12','Top_14','Top_26','Top_27','Top_28','Top_29','Top_30','Top_31','Top_32',
-    'Top_33','Top_34','Top_35','Top_55','Top_57','Top_58','Top_60','Top_61','Top_62'       
-]
-
-real_mid = ['Mid_01','Mid_02','Mid_03','Mid_04','Mid_05']
-
-real_bot = [
-    'Bot_01','Bot_02','Bot_03','Bot_04','Bot_05','Bot_06','Bot_07','Bot_08','Bot_09','Bot_10',
-    'Bot_11','Bot_14','Bot_23','Bot_26','Bot_27','Bot_33','Bot_35','Bot_55','Bot_57','Bot_59',
-    'Bot_61'  
-]
-
-dot_top = ['Top_06', 'Top_09', 'Top_27']
-
-dot_bot = ['Bot_06', 'Bot_09', 'Bot_27']
-
-line_top = []
 
 
 #loads in the known hive runes
 colnames = ['num0', 'mid0', 'num1','mid1','num2']
 data = pd.read_csv('../Hive_Rune_Database_Numbers.csv', names=colnames)
 
+
 variant = Variant(data)
 
+for num0 in range(len(num)):
+    first = num[num0] 
+    num0_im = cv2.imread(dir + "Nums/" + first + ".png")
+    for num1 in range(len(num)):
+        second = num[num1]
+        num1_im = cv2.imread(dir + "Nums/" + second + ".png")
+
+        #do small number stuff here
+        midS_im = cv2.imread(dir + 'Mids/mid_01.png')
+        small_img = np.hstack([num0_im,midS_im,num1_im])
+        search = [first,'mid_01',second]
+        save_name = '_'.join(search) + '.png'
+        SaveFunction(data,search,save_name,small_img,variant,True)
+        
+        
+        for num2 in range(len(num)):
+            third = num[num2]
+            midL_im = cv2.imread(dir + 'Mids/mid_02.png')
+            num2_im = cv2.imread(dir + "Nums/" + third + ".png")
+            large_img = np.hstack([num0_im,midL_im,num1_im,midL_im,num2_im])
+            search = [first,'mid_02',second,'mid_02',third]
+            save_name = '_'.join(search) + '.png'
+            SaveFunction(data,search,save_name,large_img,variant,False)
 
 
-
-
-
-for i in range(len(top)):
-    top_im = cv2.imread(dir + "Tops/" + top[i] + ".png")
-    for j in range(len(mid)):
-        mid_im = cv2.imread(dir + "Mids/" + mid[j] + ".png")
-        for k in range(len(bot)):
-            bot_im = cv2.imread(dir + "Bottoms/" + bot[k] + ".png")
-
-            img = np.hstack([top_im,mid_im,bot_im])
-            save_name = top[i] + '_' + mid[j]+ '_' + bot[k] + ".png"
-
-
-            for r in data:
-                
-                #if this is in the database it shouldn't be saved
-                
-                print(top[i],mid[j],bot[k])
-                
-                if ((data['tops'] == top[i]) & (data['mids'] == mid[j]) & (data['bots'] == bot[k])).any() == True:
-                    save_dir = 'Full/Real/'
-                    real = color_change(img,real = True)
-                    cv2.imwrite( save_dir + save_name,real)
-                    print("This rune is real")
-                    continue
-                
-                if top[i] in broken_top and bot[k] in broken_bot:
-                    print("This rune would break the rules!")
-                    continue
-                
-                if mid[j] == 'Mid_03' and top[i] not in circle_top and bot[k] not in circle_bot:
-                    print("This rune would break the rules!")
-                    continue
-                
-                if top[i] == 'Top_26' and bot[k] not in triangle_bot or bot[k] == 'Bot_26' and top[i] not in triangle_top:
-                    print("This rune would break the rules!")
-                    continue
-                
-                if (
-                    top[i] == 'Top_10' and mid[j] != 'Mid_04' or 
-                    top[i] == 'Top_12' and mid[j] != 'Mid_04' or
-                    top[i] == 'Top_27' and mid[j] != 'Mid_04' or  
-                    bot[k] == 'Bot_10' and mid[j] != 'Mid_04' or 
-                    bot[k] == 'Bot_12' and mid[j] != 'Mid_04' or
-                    bot[k] == 'Bot_27' and mid[j] != 'Mid_04'     
-                    ):
-                    print('This rune would break the rules')
-                    continue
-                
-                #check to see if the rune is variant
-                variant_search = [top[i],mid[j],bot[k]]
-                
-                if variant_search in variant:
-                    print("This is a Variant Rune")
-                    flip = color_change(img,flip=True)
-                    save_dir = 'Full/Variant/'
-                    cv2.imwrite( save_dir + save_name,flip)
-                
-                elif top[i] in real_top and mid[j] in real_mid and bot[k] in real_bot:
-                    if mid[j] == 'Mid_02' or mid[j] == 'Mid_05':
-                        print("This one has the wonky middle")
-                    else:
-                        print("This is a Hypothetical Rune")
-                        hypo = color_change(img,hypo=True)      
-                        save_dir = 'Full/Hypothetical/'
-                        cv2.imwrite( save_dir + save_name,hypo)
-            
-                #if this isn't in the database and the parts are fake
-                else:
-                    if mid[j] == 'Mid_02' or mid[j] == 'Mid_05':
-                        print("This one has the wonky middle")
-                    else:
-                        print("This is a Imaginary Rune")
-                        imag = color_change(img,imag=True)      
-                        save_dir = 'Full/Imaginary/'
-                        cv2.imwrite( save_dir + save_name,imag)
+            #do beeg number stuff here
